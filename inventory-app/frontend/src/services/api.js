9d4e336ad1dc9f4819f0c407ea74354c311df8be
Prabhat Prajapati <prabhauprajapati678@gmail.com>
2026-04-03 18:37:43 +0530

    Set up dynamic API environment variables

--- inventory-app/frontend/src/services/api.js
+++ inventory-app/frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Change to your production URL later
-});
+ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', // Change to your production URL later //'http://localhost:5000/api'
+});//baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
 
 // Request interceptor to add the auth token to headers
 api.interceptors.request.use((config) => {
